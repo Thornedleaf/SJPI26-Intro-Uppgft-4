47c032326b78eaf0dc3915b6dc9d73cd0fa3c451
Thornedleaf <oskar.josefsson.webd24jon@edu.tucsweden.se>
2026-09-22 16:10:35 +0200

    Uppgift 4 G

---
+++ script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById('new-task');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+const counter = document.querySelector('p');
+let completedTasks = 0;
+
+counter.textContent = `${completedTasks} completed tasks`;
+
+list.addEventListener('click', (event) => {
+	const item = event.target.closest('li');
+
+	if (!item) {
+		return;
+	}
+
+	if (item.classList.toggle('completed')) {
+		completedTasks ++;
+	} else {
+		completedTasks --;
+	}
+
+	counter.textContent = `${completedTasks} completed tasks`;
+});
+
+button.addEventListener('click', () => {
+	const text = input.value.trim();
+
+	if (text === '') {
+		return;
+	}
+
+	const item = document.createElement('li');
+	item.textContent = text;
+	list.appendChild(item);
+
+	input.value = '';
+});
